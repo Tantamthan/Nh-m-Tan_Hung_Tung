b45e1a55adb9cb26523ae31a2b3d19125bfbe376
quochungnew <npqhung2020@gmail.com>
2026-03-25 20:08:27 +0700

    Update Lap 7 clean code

---
+++ Nh-m-Tan_Hung_Tung-main/ASC.Tests/FakeSession.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class FakeSession : ISession
+{
+    private Dictionary<string, byte[]> _sessionStorage = new Dictionary<string, byte[]>();
+
+    public IEnumerable<string> Keys => _sessionStorage.Keys;
+
+    public string Id => Guid.NewGuid().ToString();
+
+    public bool IsAvailable => true;
+
+    public void Clear()
+    {
+        _sessionStorage.Clear();
+    }
+
+    public Task CommitAsync(CancellationToken cancellationToken = default)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task LoadAsync(CancellationToken cancellationToken = default)
+    {
+        return Task.CompletedTask;
+    }
+
+    public void Remove(string key)
+    {
+        _sessionStorage.Remove(key);
+    }
+
+    public void Set(string key, byte[] value)
+    {
+        _sessionStorage[key] = value;
+    }
+
+    public bool TryGetValue(string key, out byte[] value)
+    {
+        return _sessionStorage.TryGetValue(key, out value);
+    }
+}
---
+++ Nh-m-Tan_Hung_Tung-main/ASCwed/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using ASCwed.Cofiguration;
+using ASCwed.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System.Diagnostics;
+
+namespace ASCwed.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IOptions<ApplicationSettings> _settings;
+
+        public HomeController(ILogger<HomeController> logger, IOptions<ApplicationSettings> settings)
+        {
+            _logger = logger;
+            _settings = settings;
+        }
+
+        public IActionResult Index()
+        {
+          
+            HttpContext.Session.SetString("name", "Hung");
+
+        
+            var sessionValue = HttpContext.Session.GetString("name");
+
+          
+            var appName = _settings.Value.ApplicationName ?? "Default App";
+
+           
+            return View(model: appName);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            });
+        }
+    }
+}
